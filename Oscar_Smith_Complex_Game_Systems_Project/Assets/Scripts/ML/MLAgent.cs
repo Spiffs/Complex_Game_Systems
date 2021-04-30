a7c995198e9619a1ed401bd96d3ddde43b983a11
Oscar <oscarbuzzsmith@gmail.com>
2021-04-30 16:51:56 +1000

    Machine Learning 3 | 30042021 | Started Writing Code

---
+++ Oscar_Smith_Complex_Game_Systems_Project/Assets/Scripts/ML/MLAgent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CML;
+
+public class MLAgent : MonoBehaviour
+{
+    public MLInput MLInput = new MLInput();
+
+    
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+
+    }
+}
