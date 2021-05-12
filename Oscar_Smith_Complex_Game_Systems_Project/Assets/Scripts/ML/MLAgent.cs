ee82555110cf5ca9736d4509c6d470d59e5680be
Oscar <oscarbuzzsmith@gmail.com>
2021-05-12 16:54:57 +1000

    Machine Learning 6 | 12052021 | Continuing slowly

--- Oscar_Smith_Complex_Game_Systems_Project/Assets/Scripts/ML/MLAgent.cs
+++ Oscar_Smith_Complex_Game_Systems_Project/Assets/Scripts/ML/MLAgent.cs
@@ -2,19 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 using CML;
+using System.Linq;
 
 public class MLAgent : MonoBehaviour
 {
+    // input management
     public MLInput MLInput = new MLInput();
 
-    
+    public KeyCode[] Inputs;
+
+    // ai management
+    private bool begin;
+    public float reward = 0;
+    private SortedDictionary<float, SortedDictionary<float, List<KeyCode>>> savedStages
+        = new SortedDictionary<float, SortedDictionary<float, List<KeyCode>>>();
+    private float currentStage;
+
     void Start()
     {
+        foreach (KeyCode key in Inputs)
+        {
+            MLInput.AddInput(key);
+        }
 
+        StartCoroutine("TryButtons");
     }
-    
-    void Update()
-    {
 
+    void FixedUpdate()
+    {
+        if (begin)
+        {
+            if (savedStages.ContainsKey(currentStage))
+            {
+                savedStages[currentStage].Last().Key
+            }
+        }
+    }
+
+    IEnumerator TryButtons()
+    {
+        for (int i = 0; i > Inputs.Length; i++)
+        {
+            MLInput.PressKey(Inputs[i]);
+            yield return null;
+            SortedDictionary<float, List<KeyCode>> newStep = savedStages[i];
+            newStep.Add(reward, MLInput.GetKeysDown()); 
+            savedStages.Add(currentStage, newStep);
+        }
     }
 }
--- Oscar_Smith_Complex_Game_Systems_Project/Assets/Scripts/ML/MLLib.cs
+++ Oscar_Smith_Complex_Game_Systems_Project/Assets/Scripts/ML/MLLib.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 namespace CML
 {
-    public class MLInput : MonoBehaviour
-    { 
+    public class MLInput
+    {
         private Dictionary<KeyCode, bool> AvailableInputs = new Dictionary<KeyCode, bool>();
 
         public void AddInput(KeyCode newInput)
@@ -18,7 +18,7 @@ namespace CML
             AvailableInputs[pressedKey] = true;
         }
 
-        public bool GetKeyDown(KeyCode key)
+        public bool IsKeyDown(KeyCode key)
         {
             if (AvailableInputs[key] || Input.GetKeyDown(key))
             {
@@ -28,16 +28,28 @@ namespace CML
                 return false;
         }
 
+        public List<KeyCode> GetKeysDown()
+        {
+            List<KeyCode> downKeys = new List<KeyCode>();
+
+            foreach (KeyCode key in AvailableInputs.Keys)
+            {
+                if (AvailableInputs[key] == true)
+                    downKeys.Add(key);
+            }
+            return downKeys;
+        }
+
         public void Update()
         {
-            foreach(KeyValuePair<KeyCode, bool> input in AvailableInputs)
+            foreach (KeyValuePair<KeyCode, bool> input in AvailableInputs)
             {
                 AvailableInputs[input.Key] = false;
             }
         }
     }
 
-    public class MLEnvioronmentInfo : MonoBehaviour
+    public class MLEnvioronmentInfo
     {
         private Transform AITransform;
 
@@ -46,4 +58,5 @@ namespace CML
             AITransform = newTransform;
         }
     }
+
 }
